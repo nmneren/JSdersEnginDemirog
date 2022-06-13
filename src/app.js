a61acaba45dc83e3e163176a445d677e09a71d59
nmneren <wew2ggs@gmail.com>
2022-06-14 01:30:57 +0300

    14 haziran 2022 ders sonu

--- src/app.js
+++ src/app.js
@@ -1,36 +1,16 @@
-console.log("Merhaba Kodlama.io")
+console.log("merhaba dünya")
 
-//JS type safe değildir
-let dolarBugun = 9.30
-
-let dolarDun = 9.20
-dolarDun = "9.20"
+let dolardun = 9.20
+let dolarbugun = 9.30
 {
-    let dolarDun = 9.10
+    let dolardun = 9.10
 }
 
-console.log(dolarDun)
+console.log(dolardun)
 
-const euroDun = 11.2
-//euroDun = 11
-
-console.log(euroDun)
+//camelCasing -> değişken isimlendirme ilk kelime küçük sonraki gelen kelimeler büyük başlar 
 
 //array
-//camelCasing
-//PascalCasing
-let konutKredileri = ["Konut kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredisi"]
-
-console.log("<ul>")
-for(let i = 0;i<konutKredileri.length;i++){
-    console.log("<li>"+konutKredileri[i]+"</li>")
-}
-console.log("</ul>")
-
-{/* <ul>
-    <li>Konut kredisi</li>
-    <li>Emlak Konut Kredisi</li>
-    <li>Kamu Konut Kredisi</li>
-</ul> */}
+let konutKredileri = ["konut kredisi","emlak konut kredisi","komu konut kredisi"]
 
 console.log(konutKredileri)
