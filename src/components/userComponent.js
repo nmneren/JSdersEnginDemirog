04d1913113bbff6b3dfb2d2265bd78fc8b5a4383
nmneren <wew2ggs@gmail.com>
2022-06-18 01:07:53 +0300

    18 haziran ders sonu

--- src/components/userComponent.js
+++ src/components/userComponent.js
@@ -1,4 +1,5 @@
 import { BaseLogger, ElasticLogger, MongoLogger } from "../crossCuttingConcerns/logging/logger.js"
+import Customer from "../models/customer.js"
 import User from "../models/user.js"
 import UserService from "../Services/userService.js"
 
@@ -10,11 +11,19 @@ let userService = new UserService(logger1)
 let user1 = new User(1,"Engin","demiroğ","Ankara")
 let user2 = new User(2,"Baran","Gökçekli","Muğla")
 
-userService.add(user1)
-userService.add(user2)
+//userService.add(user1)
+//userService.add(user2)
 
-console.log(userService.list())
-console.log(userService.getById(2))
+console.log("-------------------------")
+userService.load()
+
+userService.add(new Customer(1,"Seda","Yılmaz","Ankara",25))
+
+console.log(userService.customers)
+console.log(userService.employees)
+console.log(userService.errors)
+
+console.log(userService.getCustomersSorted())
 
 
 
